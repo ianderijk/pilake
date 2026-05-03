d54ed2849d49f5afd890944fe6a10888e100bbbb
Ian de Rijk <ian.derijk@gmail.com>
2026-05-03 23:15:47 +0100

    handle bucket creation

--- src/pilake/connector.py
+++ src/pilake/connector.py
@@ -1,24 +1,45 @@
+from typing import Callable
 import boto3
 import os
 from dotenv import load_dotenv
-from logger import ProjectLogger
-
-logger = ProjectLogger("pilake_package")
 
 load_dotenv()
 
 s3 = boto3.client(
     "s3",
-    endpoint_url="http://localhost:9000",
+    endpoint_url="http://192.168.0.29:9000",
     aws_access_key_id=str(os.getenv("MINIO_ROOT_USER")),
     aws_secret_access_key=str(os.getenv("MINIO_PASSWORD")),
 )
 
 
+def get_buckets_list() -> list[str]:
+    buckets_data = s3.list_buckets()
+    buckets = buckets_data["Buckets"]
+    buckets_list = [x["Name"] for x in buckets]
+    return buckets_list
+
+
+buckets = get_buckets_list()
+
+
+def bucket_handler(func: Callable):
+    def wrapper(**kwargs):
+        bucket_name = kwargs.get("bucket_name", "default_bucket")
+        if bucket_name in buckets:
+            return func(**kwargs)
+        s3.create_bucket(Bucket=bucket_name)
+        buckets.append(bucket_name)
+        return func(**kwargs)
+
+    return wrapper
+
+
+@bucket_handler
 def send_to_bucket(
     bucket_name: str,
     partition_name: str,
-    _bytes: bytes,
+    bytes_: bytes,
     filename: str,
     filetype: str,
     s3=s3,
@@ -29,9 +50,9 @@ def send_to_bucket(
     Args:
         - bucket_name: The name of the bucket being posted to
         - partition_name: The name of the partition, if it doens't exist it will be created
-        - _bytes: The stream of bytes from the data being uploaded
+        - bytes_: The stream of bytes from the data being uploaded
         - filename: The name of the file as it will appear in the bucket
-        - filetype: The file extension
+        - filetype: The file extension. Must include "." prefix of extension
         - s3: The boto3 client object. A default value of the package client will be passed if not populated
 
     Returns:
@@ -40,7 +61,7 @@ def send_to_bucket(
     s3.put_object(
         Bucket=bucket_name,
         Key=f"{bucket_name}/{partition_name}/{filename}{filetype}",
-        Body=_bytes,
+        Body=bytes_,
         ContentType="application/octet-stream",
     )
 
